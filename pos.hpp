13d0097bc1f4c6ba9190bf76cd4f71d7acaa623d
IsenfireLDC <isenfireldc@gmail.com>
2021-09-07 20:17:08 -0500

    Implement printing in Coord

--- pos.hpp
+++ pos.hpp
@@ -7,12 +7,19 @@
 #ifndef _POS_HPP_
 #define _POS_HPP_
 
+#include <iostream>
+
 struct Coord {
 	int x;
 	int y;
 
 	bool operator==(Coord a) {
 		return this->x == a.x && this->y == a.y;
+	};
+
+	friend std::ostream& operator<<(std::ostream &out, const Coord &coord) {
+		out << "(" << coord.x << ", " << coord.y << ")";
+		return out;
 	};
 };
 
