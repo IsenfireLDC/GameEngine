25da11a82df67bcd728966982102c3465a11ae50
IsenfireLDC <isenfireldc@gmail.com>
2021-09-13 16:22:59 -0500

    Reduce code in Model
    Rollback feature creep

--- inc/model.hpp
+++ inc/model.hpp
@@ -11,12 +11,6 @@
 #include <vector>
 
 
-enum class ModelType {
-	None,
-	Dynamic,
-	Static
-};
-
 typedef int Color;
 struct Model {
 	char model;
@@ -31,36 +25,6 @@ struct Model {
 		out << model.model;
 		return out;
 	};
-};
-
-/**
- * A class to manage a list of models
- *
- * Provides method to add and remove models
- *
- * Stores models in a vector
- */
-class ModelManager {
-public:
-	//Constructor
-	ModelManager();
-
-	//Add model to list
-	int createModel(char, Color);	//new
-	int addModel(Model*);		//existing
-
-	//Replace existing model
-	void replaceModel(int, Model*);
-
-	//Remove model from list
-	void deleteModel(int);		//new
-	void removeModel(Model*);	//existing
-
-	//Get model
-	Model* getModel(int) const;
-
-private:
-	std::vector<Model*> models;
 };
 
 #endif
