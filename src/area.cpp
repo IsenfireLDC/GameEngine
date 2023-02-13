31bca6f80513c8be7535245837f489f773157c18
IsenfireLDC <isenfireldc@gmail.com>
2023-02-12 19:08:03 -0600

    Refactor Rect and Area
    Area is now an interface
    Rect is now RectArea and a subclass of Area

--- src/area.cpp
+++ src/area.cpp
@@ -6,50 +6,56 @@
 
 #include "area.hpp"
 
-/*
- * Constructor for default Area
- */
-Area::Area() : area() {};
 
 /*
- * Constructor for Area from rectangle
+ * Default constructor
  */
-Area::Area(Rect area) {
-	this->area = area;
+RectArea::RectArea() : c1(0, 0), c2(0, 0) {};
+
+/*
+ * Constructor for RectArea from bounding box corners
+ */
+RectArea::RectArea(Coord c1, Coord c2) {
+		this->c1 = c1;
+		this->c2 = c2;
 };
 
 /*
- * Constructor for Area from bounding box corners
+ * Returns ordered bounding box
  */
-Area::Area(Coord c1, Coord c2) : area(c1, c2) {};
+BoundingBox RectArea::getBoundingBox() const {
+	BoundingBox bb;
 
-/*
- * Getter for rect
- */
-Rect Area::getRect() const {
-	return this->area;
-};
+	if(c1.x < c2.x) {
+		bb.low.x = c1.x;
+		bb.high.x = c2.x;
+	} else {
+		bb.low.x = c2.x;
+		bb.high.x = c1.x;
+	};
 
-/*
- * Determines if this Area contains the given Coord
- *
- * Does not include edges
- */
-bool Area::contains(Coord c) const {
-	return this->contains(c, false);
+	if(c1.y < c2.y) {
+		bb.low.y = c1.y;
+		bb.high.y = c2.y;
+	} else {
+		bb.low.y = c2.y;
+		bb.high.y = c1.y;
+	};
+
+	return bb;
 };
 
 /*
  * Determenis if this Area contains the given Coord
  *
- * Includes edges if edges
+ * Does not include edges by default
  */
-bool Area::contains(Coord c, bool edges) const {
-	int a = this->area.c1.x - c.x;
-	int b = this->area.c2.x - c.x;
+bool RectArea::contains(Coord c, bool edges = false) const {
+	int a = this->c1.x - c.x;
+	int b = this->c2.x - c.x;
 	if(a*b > 0 || (!edges && a*b == 0)) return false;
 
-	a = this->area.c1.y - c.y;
-	b = this->area.c2.y - c.y;
+	a = this->c1.y - c.y;
+	b = this->c2.y - c.y;
 	return a*b < 0 || (edges && a*b == 0);
 };
