1cff63592ff9ce94197d983ef211729befe5ab86
IsenfireLDC <isenfireldc@gmail.com>
2023-04-18 14:07:41 -0500

    Create component base class and update default constructor

--- inc/component.hpp
+++ inc/component.hpp
@@ -1,28 +1,45 @@
 #ifndef _COMPONENT_HPP_
 #define _COMPONENT_HPP_
 
+#include <vector>
+#include <unordered_set>
+
+class ComponentBase;
+
 namespace Engine {
+	extern std::vector<std::unordered_set<ComponentBase*>> components;
 	extern int GCID;
 };
+
+class ComponentBase {
+public:
+	virtual int componentID() const = 0;
+
+	class Entity *entity = nullptr;
+};
+
 /*
  * Must hold some form of unique component id
  */
 template<typename T>
-class Component {
+class Component : public ComponentBase {
 public:
-	Component() {
-		this->id = getComponentID<T>();
+	Component() : id(getComponentID<T>()) {};
+
+	int componentID() const {
+		return this->id;
 	};
 
-	const int id;
-
-private:
+public:
 	//6365115
 	template<typename D>
 	static int getComponentID() {
 		static int id = ++Engine::GCID;
 		return id;
 	};
+
+private:
+	const int id;
 };
 
 #endif
--- src/component.cpp
+++ src/component.cpp
@@ -1,3 +1,5 @@
 #include "component.hpp"
 
+std::vector<std::unordered_set<ComponentBase*>> Engine::components;
+
 int Engine::GCID = 0;
