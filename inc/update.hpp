38f1a443b91cab82eeab9083af2a36a226614fa2
IsenfireLDC <isenfireldc@gmail.com>
2023-04-19 17:54:32 -0500

    Refactor the Update class to enable inheritance for the creation of different update classes

--- inc/update.hpp
+++ inc/update.hpp
@@ -3,22 +3,101 @@
 
 #include <unordered_set>
 
-class Update {
+#include "threads.hpp"
+
+#include "engine.hpp"
+
+template<typename T> class UpdateBase;
+
+/*
+ * Controller class used to perform updates on the template update class
+ *
+ * All instances for a given template type will act on the same objects, but
+ * each instance will use its own thread pool
+ */
+template<typename T>
+class UpdateController {
 public:
-	Update();
-	virtual ~Update();
+	friend UpdateBase<T>;
+
+	UpdateController(ThreadPool* = &Engine::threadPool);
+	~UpdateController();
+
+	/*
+	 * Call the update method of T type update class
+	 */
+	void update(float);
+
+private:
+	ThreadPool *threadPool;
+
+	static std::unordered_set<T*> members;
+};
+
+/*
+ * Base class for adding update support
+ *
+ * Uses CRTP to handle per-type object registration automatically
+ */
+template<typename T>
+class UpdateBase {
+public:
+	friend UpdateController<T>;
+
+	UpdateBase();
+	virtual ~UpdateBase();
 
 	virtual void update(float) = 0;
 
 protected:
 	bool doUpdate;
+};
 
 
+/*
+ * Update class for frame-based updates
+ */
+class Update : public UpdateBase<Update> {
 public:
-	static void updateAll(float);
+	Update();
+	virtual ~Update();
 
-private:
-	static std::unordered_set<Update*> members;
+	virtual void update(float) = 0;
+};
+
+
+/********** TEMPLATE METHODS **********/
+
+template<typename T> std::unordered_set<T*> UpdateController<T>::members{};
+
+/*
+ * Update all registered objects with the controller's thread pool
+ */
+template<typename T>
+void UpdateController<T>::update(float delta) {
+	for(UpdateBase<T> *update : UpdateController<T>::members) {
+		if(update->doUpdate)
+			this->threadPool->add(std::bind(update->update, update, delta));
+	};
+};
+
+
+/*
+ * Registers this object to the update controller for this type
+ */
+template<typename T>
+UpdateBase<T>::UpdateBase() {
+	UpdateController<T>::members.insert(static_cast<T*>(this));
+
+	this->doUpdate = true;
+};
+
+/*
+ * Unregisters this object from the update controller for this type
+ */
+template<typename T>
+UpdateBase<T>::~UpdateBase() {
+	UpdateController<T>::members.erase(static_cast<T*>(this));
 };
 
 #endif
--- src/update.cpp
+++ src/update.cpp
@@ -1,36 +1,12 @@
 #include "update.hpp"
 
-#include "threads.hpp"
-
-#include "engine.hpp"
-
-// Hold all active updateable objects
-std::unordered_set<Update*> Update::members;
+/*
+ * Perform initialization
+ */
+Update::Update() {};
 
 /*
- * Register this object to the update set
+ * Perform cleanup
  */
-Update::Update() {
-	Update::members.insert(this);
-};
+Update::~Update() {};
 
-/*
- * Unregister this object from the update set
- */
-Update::~Update() {
-	Update::members.erase(this);
-};
-
-
-
-/*
- * Update all registered objects
- *
- * Uses engine thread pool to run
- */
-void Update::updateAll(float delta) {
-	for(Update *update : Update::members) {
-		if(update->doUpdate)
-			Engine::threadPool.add(std::bind(update->update, update, delta));
-	};
-};
