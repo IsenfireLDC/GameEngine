2815148c0758ccfca6edfb32bbaac61d41ccb722
IsenfireLDC <isenfireldc@gmail.com>
2023-04-21 15:10:47 -0500

    Refactor Input to work with SDL

--- inc/input.hpp
+++ inc/input.hpp
@@ -7,132 +7,75 @@
 #ifndef _INPUT_HPP_
 #define _INPUT_HPP_
 
-#include "entity.hpp"
-
 #include "events.hpp"
 #include "log.hpp"
 
-#include <functional>
+#include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_keycode.h>
+
 #include <unordered_map>
-
-#include <thread>
-
-#include <string>
-
-typedef std::function<void(Entity*, int)> Action;
-//typedef void(*Action)(Entity*, int);
-
-extern Action MoveAction;
-
-extern const std::unordered_map<int, Action> defaultMap;
-
-//Special key code
-const int sKey = (224<<8);
 
 class Input {
 public:
-	enum Key : int {
-		W = 17,
-		A = 30,
-		S = 31,
-		D = 32,
-		Up = 72,
-		Left = 75,
-		Down = 80,
-		Right = 77,
-		Interrupt = 70,
-		Escape = 1,
-		Null = 0
-	};
-
-	static int getInputKey();
-	static int getInputScan();
-	static int getKeyboardLayout();
-
 	Input();
-	Input(std::unordered_map<int, Action>);
-
 	~Input();
 
-	bool spawnThread();
-	bool runThread(bool);
+	//Set the state of the key
+	void setState(SDL_Keysym&, bool);
 
-	const Action getAction(int) const;
-	bool callAction(Entity*, int) const;
+	//Get the state of the key from the scancode/keycode
+	bool pressed(SDL_Scancode) const;
+	bool pressed(SDL_Keycode) const;
 
-	void addActionMapping(int, Action);
-	void removeActionMapping(int);
+	SDL_Keymod getModifiers() const;
 
 private:
 	static Log log;
-	void threadHandler();
 
-	std::unordered_map<int, Action> actionMap;
+	std::unordered_map<SDL_Scancode, bool> scancodes;
+	std::unordered_map<SDL_Keycode, bool> keycodes;
 
-	std::thread *thread = nullptr;
-
-	int timeout = 200;
-	bool active = false;
-
-	HANDLE intrHandle;
+	SDL_Keymod modifiers;
 };
 
-struct InputEvent : Event {
-	Input::Key key;
+//struct ActionEvent : Event {
+//	Action action;
+//	int input;
+//
+//	ActionEvent(Action action, int input) : Event() {
+//		this->action = action;
+//		this->input = input;
+//	};
+//
+//	virtual int getHash() {
+//		return Event_MgetHash(ActionEvent);
+//	};
+//
+//	virtual std::string getInfo() {
+//		char info[100];
+//		void(**act)(Entity*,int) = this->action.target<void(*)(Entity*,int)>();
+//		sprintf(info, "ActionEvent{action=%p;input=%d}", *act, this->input);
+//		return std::string(info);
+//	};
+//};
 
-	InputEvent(Input::Key key) : Event() {
-		this->key = key;
-	};
-
-	virtual int getHash() {
-		return Event_MgetHash(InputEvent);
-	};
-
-	virtual std::string getInfo() {
-		char info[100];
-		sprintf(info, "InputEvent{key=%d}", this->key);
-		return std::string(info);
-	};
-};
-
-struct ActionEvent : Event {
-	Action action;
-	int input;
-
-	ActionEvent(Action action, int input) : Event() {
-		this->action = action;
-		this->input = input;
-	};
-
-	virtual int getHash() {
-		return Event_MgetHash(ActionEvent);
-	};
-
-	virtual std::string getInfo() {
-		char info[100];
-		void(**act)(Entity*,int) = this->action.target<void(*)(Entity*,int)>();
-		sprintf(info, "ActionEvent{action=%p;input=%d}", *act, this->input);
-		return std::string(info);
-	};
-};
-
-struct QuitEvent : Event {
-	Input *input;
-
-	QuitEvent(Input *input) : Event() {
-		this->input = input;
-	};
-
-	virtual int getHash() {
-		return Event_MgetHash(QuitEvent);
-	};
-
-	virtual std::string getInfo() {
-		char info[100];
-		sprintf(info, "QuitEvent{input=%p}", this->input);
-		return std::string(info);
-	};
-};
+//struct QuitEvent : Event {
+//	Input *input;
+//
+//	QuitEvent(Input *input) : Event() {
+//		this->input = input;
+//	};
+//
+//	virtual int getHash() {
+//		return Event_MgetHash(QuitEvent);
+//	};
+//
+//	virtual std::string getInfo() {
+//		char info[100];
+//		sprintf(info, "QuitEvent{input=%p}", this->input);
+//		return std::string(info);
+//	};
+//};
 
 //TODO?: Create async handler for input with new event system
 
--- src/input.cpp
+++ src/input.cpp
@@ -6,249 +6,56 @@
 
 #include "input.hpp"
 
-#include <conio.h>
-#include <winuser.h>
+#include "log.hpp"
 
 #include <sstream>
 
 #include "engine.hpp"
 
-Log Input::log{"Input", "./logs/input.log"};
+Log Input::log{"Input", "./logs/input.log", LogLevel::Debug};
 
-static void aMove(Entity *target, int input) {
-	Coord ePos = target->getPos();
+Input Engine::input{};
 
-	switch (input) {
-		case Input::Key::W:
-		case Input::Key::Up:
-			ePos = ePos + Coord(0,-1);
-			break;
-		case Input::Key::A:
-		case Input::Key::Left:
-			ePos = ePos + Coord(-1,0);
-			break;
-		case Input::Key::S:
-		case Input::Key::Down:
-			ePos = ePos + Coord(0,1);
-			break;
-		case Input::Key::D:
-		case Input::Key::Right:
-			ePos = ePos + Coord(1,0);
-			break;
-	};
 
-	target->move(ePos);
+Input::Input() : scancodes(), keycodes(), modifiers(KMOD_NONE) {};
+
+Input::~Input() {};
+
+
+/*
+ * Used to set the state of the key
+ */
+void Input::setState(SDL_Keysym &key, bool pressed) {
+	this->scancodes[key.scancode] = pressed;
+	this->keycodes[key.sym] = pressed;
+
+	this->modifiers = (SDL_Keymod)key.mod;
+
+	std::stringstream key_s;
+	key_s << "Key " << key.scancode << " set to state " << pressed;
+
+	Input::log << key_s.str();
 };
 
-static void aExit(Entity *target, int input) {
-	std::cerr << "Keyboard interrupt" << std::endl;
-	Engine::log.log("Keyboard interrupt", LogLevel::Fatal, "Input");
 
-	exit(1);
-};
-
-Action MoveAction = aMove;
-Action ExitAction = aExit;
-
-const std::unordered_map<int, Action> defaultMap = {
-	{Input::Key::W, MoveAction},
-	{Input::Key::A, MoveAction},
-	{Input::Key::S, MoveAction},
-	{Input::Key::D, MoveAction},
-	{Input::Key::Up, MoveAction},
-	{Input::Key::Left, MoveAction},
-	{Input::Key::Down, MoveAction},
-	{Input::Key::Right, MoveAction}
-};
-
-static INPUT_RECORD getInput(int timeout, HANDLE interruptHandle) {
-	const int bufferSize = 10;
-
-	DWORD mode;
-
-	static INPUT_RECORD input[bufferSize];
-	static int buffered = 0;
-
-	HANDLE waitHandles[2] = {nullptr, interruptHandle};
-
-	if(buffered == 0) {
-		//Std handle
-		HANDLE handle = GetStdHandle(STD_INPUT_HANDLE);
-
-		//Save current mode
-		GetConsoleMode(handle, &mode);
-
-		//Set mode
-		SetConsoleMode(handle, 0);
-		waitHandles[0] = handle;
-		if(WaitForMultipleObjects(2, waitHandles, false, timeout) == WAIT_OBJECT_0) {
-			DWORD cnt;
-
-			//Get input event
-			ReadConsoleInput(handle, input, bufferSize, &cnt);
-
-			buffered = cnt;
-		} else {
-			input[0].EventType = 0;
-			SetConsoleMode(handle, mode);
-			return input[0];
-		};
-
-		SetConsoleMode(handle, mode);
-	};
-
-
-	INPUT_RECORD retVal = input[0];
-
-	--buffered;
-
-	for(int i = 0; i < buffered; ++i)
-		input[i] = input[i+1];
-
-	return retVal;
-};
-
-int Input::getInputKey() {
-	INPUT_RECORD input = getInput(0, 0);
-
-	if(input.EventType == KEY_EVENT)
-		return getInput(0, 0).Event.KeyEvent.wVirtualKeyCode;
-
-	return 0;
-};
-
-int Input::getInputScan() {
-	INPUT_RECORD input = getInput(0, 0);
-
-	if(input.EventType == KEY_EVENT)
-		return getInput(0, 0).Event.KeyEvent.wVirtualScanCode;
-
-	return 0;
-};
-
-Input::Input() : Input(defaultMap) {};
-
-Input::Input(std::unordered_map<int, Action> actionMap) {
-	this->actionMap = actionMap;
-
-	this->intrHandle = CreateEventA(0, true, false, 0);
+/*
+ * Gets the state of a key from the scancode
+ */
+bool Input::pressed(SDL_Scancode key) const {
+	return this->scancodes.at(key);
 };
 
 /*
- * Wait for thread to finish on delete
+ * Gets the state of a key from the keycode
  */
-Input::~Input() {
-	this->active = false;
-
-	PulseEvent(this->intrHandle);
-	if(this->thread->joinable()) this->thread->join();
+bool Input::pressed(SDL_Keycode key) const {
+	return this->keycodes.at(key);
 };
 
-/*
- * Spawn a thread for this instance
- *
- * Returns if a thread exists after the call
- */
-bool Input::spawnThread() {
-	if(this->thread && this->thread->joinable()) return true;
-
-	this->active = true;
-	this->thread = new std::thread(&Input::threadHandler, this);
-	
-	return this->thread->joinable();
-};
 
 /*
- * Pause/resume the thread if it exists, otherwise return false
+ * Get the current key modifiers (ctrl, alt, shift, etc.)
  */
-bool Input::runThread(bool run) {
-	if(!this->thread->joinable()) return false;
-
-	//TODO: Pause/resume execution of thread
-	this->active = run;
-	return true;
-};
-
-/*
- * Get the Action associated with input
- */
-const Action Input::getAction(int input) const {
-	if(this->actionMap.find(input) != this->actionMap.end())
-		return this->actionMap.at(input);
-
-	return nullptr;
-};
-
-/*
- * Call an action on target (automatically resolves action)
- */
-bool Input::callAction(Entity *target, int input) const {
-	Action a = this->getAction(input);
-
-	if(a) {
-		a(target, input);
-		return true;
-	} else return false;
-};
-
-/*
- * Inserts or updates the Action mapped to input
- */
-void Input::addActionMapping(int input, Action action) {
-	this->actionMap[input] = action;
-};
-
-/*
- * Removes the Action mapped to input
- */
-void Input::removeActionMapping(int input) {
-	if(this->actionMap.find(input) != this->actionMap.end())
-		this->actionMap.erase(input);
-};
-
-/*
- * Input handler
- *
- * Thread exits on generating QuitEvent
- */
-void Input::threadHandler() {
-	Input::log.log("Input thread started", LogLevel::Info, "Handler");
-	while(this->active) {
-		INPUT_RECORD input = getInput(10000, this->intrHandle);
-		int scanCode;
-
-		if(input.EventType == KEY_EVENT && input.Event.KeyEvent.bKeyDown)
-			scanCode = input.Event.KeyEvent.wVirtualScanCode;
-		else
-			scanCode = 0;
-	
-		const Action action = this->getAction(scanCode);
-
-		Event *event;
-		if(action) {
-			event = new ActionEvent(action, scanCode);
-		} else {
-			switch(scanCode) {
-				case Input::Key::Escape:
-					event = new QuitEvent(this);
-					break;
-				case Input::Key::Interrupt:
-					aExit(nullptr, 0);
-				case Input::Key::Null:
-					continue;
-				default:
-					event = new InputEvent((Input::Key)scanCode);
-			};
-		};
-		Engine::eventBus.queueEvent(event);
-
-		std::stringstream key{};
-		key << "Received keypress ";
-		if(input.Event.KeyEvent.wVirtualKeyCode > 16) key << "\"" << (char)input.Event.KeyEvent.wVirtualKeyCode << "\" ";
-		key << ": " << scanCode;
-		Input::log.log(key.str(), LogLevel::Debug, "Handler");
-	
-		//Select action and queue event
-		//Have quit event contain pointer to Input
-	}
+SDL_Keymod Input::getModifiers() const {
+	return this->modifiers;
 };
