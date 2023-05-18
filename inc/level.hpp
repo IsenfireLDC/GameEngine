8504ce84f826d0c8e92c3e782baded9a81400c81
IsenfireLDC <isenfireldc@gmail.com>
2023-05-18 17:16:32 -0500

    Implement Level class

--- inc/level.hpp
+++ inc/level.hpp
@@ -2,6 +2,7 @@
 #define _LEVEL_HPP_
 
 #include <unordered_set>
+#include <string>
 
 class Level {
 public:
@@ -11,8 +12,17 @@ public:
 	void addEntity(class Entity*);
 	void removeEntity(class Entity*);
 
+	bool hasEntity(class Entity*) const;
+	class Entity* findEntity(std::string) const;
+
 private:
 	std::unordered_set<class Entity*> entities;
 };
+
+/*
+ * TODO: In component?
+ * Add location-based entity search
+ * Add component-based entity search
+ */
 
 #endif
--- src/level.cpp
+++ src/level.cpp
@@ -2,129 +2,52 @@
 
 #include "entity.hpp"
 
-/*
- * Constructor for default entity manager
- */
-Level::Level() {
-	this->field = nullptr;
-	this->entities = std::vector<Entity*>();
-};
+#include "engine.hpp"
+
+Level Engine::level{};
 
 /*
- * Constructor for new entity manager
+ * Create new level
+ * Initialize empty set of entities
  */
-Level::Level(Field* field) {
-	this->field = field;
-
-	this->field->setScale(Coord(2,1));
-
-	this->entities = std::vector<Entity*>();
-};
+Level::Level() {};
 
 /*
  * Destructor
- *
- * Unregisters all entities
  */
-Level::~Level() {
-	for(int i = this->entities.size()-1; i >= 0; --i)
-		this->unregisterEntity(this->entities[i]);
+Level::~Level() {};
+
+
+/*
+ * Add an entity to the level
+ */
+void Level::addEntity(Entity *entity) {
+	this->entities.insert(entity);
+};
+
+/*
+ * Remove an entity from the level
+ */
+void Level::removeEntity(Entity *entity) {
+	this->entities.erase(entity);
 };
 
 
 /*
- * Finds first entity at given coordinates (1 max at coords)
- *
- * Returns null pointer if no entity found
+ * Checks if the given entity exists in this level
  */
-Entity* Level::getEntityAt(Coord pos) const {
-	for(Entity* entity : this->entities)
-		if(pos == entity->pos)
+bool Level::hasEntity(Entity *entity) const {
+	return this->entities.count(entity) > 0;
+};
+
+/*
+ * Finds an entity with the given name
+ * TODO: Add other systems to search for a specific Entity*; type/id
+ */
+Entity* Level::findEntity(std::string name) const {
+	for(Entity *entity : this->entities)
+		if(entity->name == name)
 			return entity;
 
 	return nullptr;
-};
-
-/*
- * Get copy of vector containing all entities
- */
-std::vector<Entity*> Level::getEntities() const {
-	return this->entities;
-};
-
-/*
- * Add an entity to the entity manager
- *
- * Returns false if an entity already exists in the same location
- */
-bool Level::registerEntity(Entity* entity) {
-	if(entity == nullptr || !this->canPlaceAt(entity->pos)) return false;
-
-	this->entities.push_back(entity);
-
-	entity->manager = this;
-
-	return true;
-};
-
-/*
- * Remove an entity from the entity manager
- *
- * Returns false if entity was null or not found
- */
-bool Level::unregisterEntity(Entity* entity) {
-	if(entity == nullptr) return false;
-
-	unsigned int index = this->findEntity(entity);
-	if(index == this->entities.size()) return false;
-
-	this->entities.erase(this->entities.begin() + index);
-
-	entity->manager = nullptr;
-
-	return true;
-};
-
-/*
- * Attempt to move an entity to a new coordinate
- *
- * Returns false if entity is null, else returns move(pos)
- */
-bool Level::moveEntity(Entity* entity, Coord pos) {
-	if(entity == nullptr) return false;
-
-	//Call entity's move method
-	return entity->move(pos);
-};
-
-/*
- * Determines if coordinate is in field bounds
- */
-bool Level::inBounds(Coord pos) const {
-	if(this->field) return this->field->contains(pos);
-
-	return false;
-};
-
-/*
- * Determines if an entity exists at a given location
- */
-bool Level::canPlaceAt(Coord pos) const {
-	if(this->field && !this->field->contains(pos)) return false;
-
-	for(Entity* entity : this->entities)
-		if(pos == entity->pos)
-			return false;
-
-	return true;
-};
-
-/*
- * Determines where entity exists, size if it does not
- */
-unsigned int Level::findEntity(Entity *entity) const {
-	for(unsigned int i = 0; i < this->entities.size(); ++i)
-		if(this->entities[i] == entity) return i;
-
-	return this->entities.size();
 };
